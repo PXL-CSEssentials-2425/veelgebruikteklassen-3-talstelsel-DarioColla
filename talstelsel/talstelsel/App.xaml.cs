70daa2db753bd1049ef321e12a2f75bf876dac99
DarioColla <12202253@student.pxl.be>
2024-10-21 18:11:38 +0200

    test

---
+++ talstelsel/talstelsel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace talstelsel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
